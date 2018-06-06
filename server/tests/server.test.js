2e3e0a8f7907d88b4e8708a45d529b9557124634
Victor Valdez <victorvaldez5@gmail.com>
2018-06-05 23:55:36 -0500

    created Delete and Update methods and tests for delete and update

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -101,3 +101,105 @@ describe('GET /todos/:id', (done)=> {
     .end(done);
   });
 });
+
+describe('DELETE /todos/:id', (done) => {
+  //Test delete successful
+  it('Should send a 404 and a todo that got deleted successfully', (done) => {
+    var hexID = todos[0]._id.toHexString();
+    request(app)
+      .delete(`/todos/${hexID}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexID);
+      })
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+
+        Todo.findById(hexID).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      })
+  })
+
+  it('Should send a 404 when the object cannot be found ', (done) => {
+    request(app)
+      .delete(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('Should retrieve a 400 when passed an invalid id', (done) => {
+    request(app)
+      .delete('/todos/123abc')
+      .expect(404)
+      .end(done);
+  });
+});
+
+describe('PATCH /todos/:id', (done) => {
+  //Test delete successful
+  it('Should send a 200 and return a todo item with an updated completed, text, and completedAt', (done) => {
+    var hexID = todos[0]._id.toHexString();
+    var body = { "text": 'Hello', "completed": true };
+    request(app)
+      .patch(`/todos/${hexID}`)
+      .send(body)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexID);
+      })
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+
+        Todo.findByIdAndUpdate(hexID).then((todo) => {
+          expect(todo.completed).toBe(true);
+          expect(todo.text).toBe(body.text);
+          expect(todo.completedAt).toBeA('number');
+          done();
+        }).catch((e) => done(e));
+      })
+  });
+
+  it('Should send a 200 and with a null completedAt field', (done) => {
+    var hexID = todos[0]._id.toHexString();
+    var body = { "text": 'Hello', "completed": false };
+    request(app)
+      .patch(`/todos/${hexID}`)
+      .send(body)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexID);
+      })
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+
+        Todo.findByIdAndUpdate(hexID).then((todo) => {
+          expect(todo.completed).toBe(false);
+          expect(todo.text).toBe(body.text);
+          expect(todo.completedAt).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      })
+  })
+
+  it('Should send a 404 when the object cannot be found ', (done) => {
+    request(app)
+      .patch(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('Should retrieve a 400 when passed an invalid id', (done) => {
+    request(app)
+      .patch('/todos/123abc')
+      .expect(404)
+      .end(done);
+  });
+});

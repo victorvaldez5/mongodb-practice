7a47f81092e7df840861a060ca43581f7d231239
Victor Valdez <victorvaldez5@gmail.com>
2018-06-06 20:34:56 -0500

    created user authentication and a public route to get user profile

--- server/models/user.js
+++ server/models/user.js
@@ -32,6 +32,7 @@ var UserSchema = new mongoose.Schema({
   }]
 });
 
+// methods makes functions that can be used only the instance model
 // override toJSON method so that it leaves out the password when it is returned
 UserSchema.methods.toJSON = function() {
   var user = this;
@@ -51,6 +52,24 @@ UserSchema.methods.generateAuthToken = function () {
     return token;
   })
 };
+
+// statics makes model methods that can be used by all Models
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+}
 
 var User = mongoose.model('User', UserSchema);
 
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {authenticate} = require('./middleware/authenticate');
 var {ObjectID} = require('mongodb');
 var mongoose = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -104,6 +105,10 @@ app.post('/users', (req, res) => {
   }); 
 });
 
+
+app.get('/users/me', authenticate, (req, res) => {
+ res.send(req.user);
+});
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
